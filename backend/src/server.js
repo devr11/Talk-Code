c4f5ad23c812bac21954c63091f821ba4c541f5a
Dev <rastogi.db1@gmail.com>
2025-12-10 00:41:17 +0530

    modified

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,27 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { ENV } from "./lib/env.js";
-const path = require("path");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-
-const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success from api" });
 });
 
-//make our app ready for deployment
+// This part is only needed if the backend is serving the frontend build.
+// If Vercel serves your frontend, you can actually delete this block.
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-app.listen(ENV.PORT, () => console.log("Server is running on port 3000"));
+app.listen(ENV.PORT, () =>
+  console.log(`Server is running on port ${ENV.PORT}`)
+);

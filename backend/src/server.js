37400bbdd31e71603bb5692a1d5c2c45537d8cbd
Dev <rastogi.db1@gmail.com>
2025-12-09 10:21:13 +0530

    modified

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "success from api" });
 });
 
 //make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  })
 }
 
 app.listen(ENV.PORT, () => console.log("Server is running on port 3000"));

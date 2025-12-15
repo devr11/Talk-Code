b912f9ba2f33b8840ef99e2687622179a6446f8e
Dev <rastogi.db1@gmail.com>
2025-12-16 00:28:21 +0530

    modified

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,9 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env";
+
+const apiKey = ENV.STREAM_API_KEY
+const secretKey = ENV.STREAM_API_SECRET
+
+if(!apiKey || !secretKey){
+    console.error("Stream API_KEY or SECRET is missing")
+}

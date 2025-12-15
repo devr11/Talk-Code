14ffba0fa13c8733efc44e2736d81506f4579bda
Dev <rastogi.db1@gmail.com>
2025-12-15 17:23:20 +0530

    modified

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,3 +1,10 @@
 import User from "../models/User.js";
 import { connectDB } from "./db.js";
 import Inngest from "inngest"
+
+export const inngest = new Inngest({ id: "talent-iq" });
+
+const syncUser = inngest.createFunction(
+    {id:"sync-user"},
+    {event:"clerk/user,created"}
+)

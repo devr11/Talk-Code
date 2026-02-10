d9d1be20b15e7c61150d2c642d9095abc58306aa
Dev <rastogi.db1@gmail.com>
2026-02-10 19:22:50 +0530

    add a function

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,9 +6,21 @@ export const protectRoute = [
     async (req, res, next) => {
         try {
             const clerkId = req.auth().userId // here we got the id
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized"})
+            if(!clerkId) return res.status(401).json({msg: "Unauthorized - invalid token"})
+
+            // find user in db by clerkId
+            const user = await User.findOne({clerkId})
+
+            if(!user) return res.status(404).json({msg:"User not found"})
+
+            req.user = user
+
+            next(
+
+            )
         } catch (error) {
-            
+            console.error("Error in protectRoute middleware", error)
+            res.status(500).json({msg: "Internal Server Error"})
         }
     }
 ]

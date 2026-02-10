2abce7b8763e2926fddf535e4d40f72b0c3f6a7b
Dev <rastogi.db1@gmail.com>
2026-02-11 00:21:06 +0530

    add user data

--- backend/src/controllers/chatController.js
+++ backend/src/controllers/chatController.js
@@ -2,7 +2,12 @@ import { chatClient } from "../lib/stream.js";
 
 export async function getStreamToken(req, res) {
     try {
+        // use clerkId for stream not mongodb id => it should match the id we have in the stream dashboard
         const token = chatClient.createToken(req.user.clerkId)
+
+        res.status(200).json({
+            
+        })
     } catch (error) {
         
     }

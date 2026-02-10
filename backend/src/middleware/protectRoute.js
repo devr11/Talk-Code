62ba8bee1dfe662023cace496c1de98b8e929509
Dev <rastogi.db1@gmail.com>
2026-02-10 19:17:14 +0530

    add a function

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -5,7 +5,8 @@ export const protectRoute = [
     requireAuth(),
     async (req, res, next) => {
         try {
-            
+            const clerkId = req.auth().userId // here we got the id
+            if(!clerkId) return res.status(401).json({msg: "Unauthorized"})
         } catch (error) {
             
         }

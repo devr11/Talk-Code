71cb1eb4a792c41f2ee08d39cdee0c1988d0516e
Dev <rastogi.db1@gmail.com>
2026-02-10 19:48:50 +0530

    add a protected route

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import { protectRoute } from "./middleware/protectRoute.js";
 
 // Resolve __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -49,8 +50,10 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success from api - by Dev Rastogi" });
 });
+
+// when you pass an array of middleware to express, it automatically flattens and executes them sequentially, one by one
 app.get("/video-calls", protectRoute, (req, res) => {
-  res.status(200).json({ msg: "success from api - by Dev Rastogi" });
+  res.status(200).json({ msg: "this is a protected route" });
 });
 
 

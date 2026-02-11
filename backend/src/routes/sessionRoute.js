48895b6e27d44ea365e8d21a604e036764233595
Dev <rastogi.db1@gmail.com>
2026-02-11 16:42:53 +0530

    set routes

--- backend/src/routes/sessionRoute.js
+++ backend/src/routes/sessionRoute.js
@@ -9,5 +9,7 @@ router.get("/my-recent", protectRoute, getMyRecentSessions)
 
 
 router.get("/:id", protectRoute, getSessionById)
+router.post("/:id/join", protectRoute, joinSession)
+router.post("/:id/end", protectRoute, endSession)
 
 export default router

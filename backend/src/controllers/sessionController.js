5ee72a891c2ea2b2248cd89fd470d7f55a44c635
Dev <rastogi.db1@gmail.com>
2026-02-11 18:24:10 +0530

    create a func to createSession

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -1,6 +1,51 @@
-export async function createSession(req, res) {}
+import { chatClient, streamClient } from "../lib/stream.js"
+import Session from "../models/Session.js"
+
+export async function createSession(req, res) {
+    try {
+        const {problem, difficulty} = req.body
+        const userId =  req.user._id
+        const clerkId =  req.user.clerkId
+
+        if(!problem || ! difficulty){
+            return res.status(400).json({msg: "Problem and difficulty are required"})
+        }
+
+        // generate a unique call id for stream video
+        const callId = `session_${Date.now()}_${Math.random().toString(36).substring(7)}`
+
+        // create session in db
+        const session = Session.create({problem, difficulty, host: userId, callId})
+
+        // create a stream video call
+        await streamClient.video.call("default",callId).getOrCreate({
+            data:{
+                created_by_id: clerkId,
+                custom: {problem, difficulty, sessionId:session._id.toString()}
+            }
+        })
+
+        // chat messaging
+        const channel = chatClient.channel("messaging", callId, {
+            name:`${problem} Session`,
+            created_by_id: clerkId,
+            members: [clerkId]
+        })
+
+        await channel.create()
+        res.status(200).json({session})
+    } catch (error) {
+        console.error("Error in createSession controller:", error.msg)
+        res.status(500).json({msg: "Internal Server Error"})
+    }
+}
+
 export async function getActiveSessions(req, res) {}
+
 export async function getMyRecentSessions(req, res) {}
+
 export async function getSessionById(req, res) {}
+
 export async function joinSession(req, res) {}
+
 export async function endSession(req, res) {}

162ebf9355f9b45a29287b2abcd13c9b6c71699f
Dev <rastogi.db1@gmail.com>
2026-02-12 23:35:30 +0530

    implementing tanstack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,27 @@ import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { isSignedIn } = useUser();
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/problems"
-        element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/problems"
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+    </>
   );
 }
 
 export default App;
+
+// TW, daisyui, rr, rht
+// todo: setup react-query aka tanstack, axios
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/clerk-react"
+import toast from "react-hot-toast"
 
 function HomePage() {
+
+    // fetch some data without using tanstack
+    
+
   return (
     <div>
+        <button className="btn btn-secondary" onClick={()=> toast.success("This is a success toast")}>Click Me</button>
         <SignedOut>
             <SignInButton>
                 <button className="btn btn-primary">LogIn</button>

7d8495a66d4b036102b65589e241cc28a2f75f55
Dev <rastogi.db1@gmail.com>
2026-02-13 23:01:17 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,25 @@ import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProblemsPage from "./pages/ProblemsPage";
 import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect when reload the page
+  if (!isLoaded) return null;
 
   return (
     <>
       <Routes>
-        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"}/>} />
-        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"}/>} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
@@ -24,6 +34,3 @@ function App() {
 }
 
 export default App;
-
-// TW, daisyui, rr, rht
-// todo: setup react-query aka tanstack, axios
--- frontend/src/pages/ProblemsPage.jsx
+++ frontend/src/pages/ProblemsPage.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function ProblemsPage() {
   return (
     <div>ProblemsPage</div>
